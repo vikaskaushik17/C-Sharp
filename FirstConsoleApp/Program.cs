4ef6715806e0211706630c27117b2817205d4964
vikaskaushik17 <vikaskaushik00008@gmail.com>
2021-02-10 18:21:38 +0530

    C sharp first project

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please press the enter to continue..");
+           
+
+            /*
+             * This is a change
+             */
+        }
+    }
+}
