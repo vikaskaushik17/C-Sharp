d985914fbe5ce5aa76d45c6fa6d06e51e90712f0
Vikas Kaushik <vikaskaushik00008@gmail.com>
2021-02-17 11:52:57 +0530

    Try - Catch block

---
+++ TryCatch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EssentialTrainingApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadTextFile();
+
+        }
+
+        public static void ReadTextFile()
+        {
+            try
+            {
+
+                using (var sr = new StreamReader(@"A:\file1.txt")) //Text file location to read
+                {
+                    string contents = sr.ReadToEnd();
+                    Console.WriteLine(contents);
+                
+                }
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Could not file the file");
+            }catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("File not found");
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finally run all the time");
+            }
+        }
+
+    }
+}
+
+    
+
+
+
+
+
